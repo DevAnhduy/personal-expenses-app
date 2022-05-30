3544e660efd5af71abba513c8f20706bfa4091fe
Anh Duy <duypa@spaceaa.com>
2022-05-30 19:29:20 +0700

    Update[Split widget]

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './widgets/transaction_list.dart';
 
-import './transaction.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -80,43 +78,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-              children: transactions.map((tx) {
-            return Card(
-                child: Row(
-              children: [
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount.toString()}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Color.fromARGB(255, 6, 4, 7),
-                      ),
-                    )),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tx.title,
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      DateFormat('d-M-y').format(tx.date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ));
-          }).toList())
+          TransactionList(),
         ],
       ),
     );
